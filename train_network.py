18360a67daf269320c6c9c716c49b9f3ba6885e9
DKnight54 <126916963+DKnight54@users.noreply.github.com>
2025-05-18 16:50:38 +0800

    Update train_network.py
    Distributed training does not work if self.cache_text_encoder_outputs_if_needed() is not executed

--- train_network.py
+++ train_network.py
@@ -873,10 +873,9 @@ class NetworkTrainer:
     
             # 必要ならテキストエンコーダーの出力をキャッシュする: Text Encoderはcpuまたはgpuへ移される
             # cache text encoder outputs if needed: Text Encoder is moved to cpu or gpu
-            if args.cache_text_encoder_outputs:
-                self.cache_text_encoder_outputs_if_needed(
-                    args, accelerator, unet, vae, tokenizers, text_encoders, train_dataset_group, weight_dtype
-                )
+            self.cache_text_encoder_outputs_if_needed(
+                args, accelerator, unet, vae, tokenizers, text_encoders, train_dataset_group, weight_dtype
+            )
         # For --sample_at_first
         self.sample_images(accelerator, args, 0, global_step, accelerator.device, vae, tokenizer, text_encoder, unet)
 
