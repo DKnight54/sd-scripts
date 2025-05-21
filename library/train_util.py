0d3b49c28ebe92110f04b1763bc9ccb03a4a4b4d
DKnight54 <126916963+DKnight54@users.noreply.github.com>
2025-05-22 01:59:32 +0800

    Update train_util.py

--- library/train_util.py
+++ library/train_util.py
@@ -1864,6 +1864,7 @@ class DreamBoothDataset(BaseDataset):
         first_loop = True
         logger.info(f"self.reg_infos_index at: {self.temp_index}\n reg_infos_index len = {len(self.reg_infos_index)}")
         reg_img_log = f"\nDataset seed: {self.seed.value}"
+        start_index = self.temp_index
         while n < self.num_train_images :
             info, subset = temp_reg_infos[self.reg_infos_index[self.temp_index]]
             if first_loop:
