e90bb8da25769410cee5fbd401f03e93333ac99d
DKnight54 <126916963+DKnight54@users.noreply.github.com>
2025-05-24 19:59:27 +0800

    Update train_util.py

--- library/train_util.py
+++ library/train_util.py
@@ -1865,7 +1865,7 @@ class DreamBoothDataset(BaseDataset):
             for img_key in img_keys:
                 counter += self.image_data[img_key].num_repeats
             count_str += f"{counter}/{subset.img_count * subset.num_repeats}"
-            count_str += f"\nSubset dir: {subset.image_dir}" if subset.image_dir is not None else "")
+            count_str += f"\nSubset dir: {subset.image_dir}" if subset.image_dir is not None else ""
         count_str += "\n"
         logger.info(count_str)
     
